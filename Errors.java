984934a96fa5b9278a8e13269c251b728eec9934
rileyherrmann <riley.herrmann@mcusd709.org>
2024-08-21 15:57:47 -0500

    fixed errors

--- Errors.java
+++ Errors.java
@@ -1,18 +1,18 @@
 // This code is riddled with errors.
 // Please find and fix all of the errors without changing the intended purpose of the code.
 
-Public class Error{
-  public static main(string[] args){
-    String name = "Alan"
-    string animal = "duck";
-    System.outprintln("My name is " + Name + " and I am a " + animal + ".");
+public class Errors{
+  public static void main(String[] args){
+    String name = "Alan";
+    String animal = "duck";
+    System.out.println("My name is " + name + " and I am a " + animal + ".");
 
-    Int weight = 90;
+    int weight = 90;
     int volume = 8;
-    double density = weight/volume; // Should be the correct precise value.
+    double density = (double)weight/volume; // Should be the correct precise value.
 
-    System.out.pintln("I am a heavy " + animal + " because I have a density of " + dense + " kilograms per cubic centimeter.");
+    System.out.println("I am a heavy " + animal + " because I have a density of " + density + " kilograms per cubic centimeter.");
 
-    System.out.println("It was nice meeting you!")
+    System.out.println("It was nice meeting you!");
   }
 }
